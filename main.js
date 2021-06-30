2e7e6b29e9bf218aeb65a4350721f6876669e547
Mequidi <nikhilsejwal234@gmail.com>
2021-06-30 14:36:23 +0530

    banner made

--- main.js
+++
@@ -1,7 +0,0 @@
-const logoHero = document.querySelector(".logo-hero");
-console.log(screen.width)
-if(window.screen.width <= 830)
-{
-    logoHero.src = "/images/invis coffee logo (1).png"
-    console.log("width is less than 830px");
-}
