d4a4d86a49f16df31592c8fcefbbc456b6d16282
Mequidi <nikhilsejwal234@gmail.com>
2021-07-08 14:59:27 +0530

    email message

--- main.js
+++ main.js
@@ -19,10 +19,26 @@ const logoHero = document.querySelector(".logo-hero");
 const navigation = document.querySelector(".navigation");
 const links = document.querySelector(".links");
 const allLinks = document.querySelectorAll(".link-item");
+const topBtn = document.querySelector(".back-to-top");
+const submitEmail = document.querySelector(".submit-email-btn");
+
+const alertMessage = document.getElementById("alert-message");
+const input = document.getElementById("input");
+const submitForm = document.getElementById('submit-form');
 
 // quote btn functionality
 let countForQuotes = 0;
 let containerHeight = 0;
+
+window.addEventListener("load",()=>{
+    if(alertMessage.textContent=="")
+    alertMessage.style.display="none";
+})
+
+submitForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+})
+
 
 quoteBtns.forEach((btn,index) =>{
     btn.addEventListener("click",()=>{
@@ -60,11 +76,14 @@ window.addEventListener("scroll",()=>{
         navigation.classList.add("fixed-nav");
     else    
         navigation.classList.remove("fixed-nav");
+    if(scrollLength>500)
+        topBtn.classList.add("show-btn");
+    else
+        topBtn.classList.remove("show-btn");
 })
 // end fixed navbar
 //toggle navbar
-document.querySelector(".toggle-btn").addEventListener("click",()=>{
-    console.log("btn clicked!");
+document.querySelector(".toggle-btn").addEventListener("click",(e)=>{
     // links.classList.toggle("show-links");
     allLinks.forEach(function(item){
         containerHeight += item.getBoundingClientRect().height;
@@ -79,5 +98,87 @@ document.querySelector(".toggle-btn").addEventListener("click",()=>{
         links.style.height = `0px`;
         containerHeight = 0;
         links.classList.remove("show-links");
-    } 
+    }
 })
+
+submitEmail.addEventListener("click",()=>{
+    const inputText = input.value;
+    console.log(inputText)
+    if(!input.value)
+    {
+        displayMessage("Email left empty!","danger")
+    }
+    else
+    if(inputText.search("@")===-1)
+    {
+        displayMessage("please include an `@` in the email","danger")
+    }
+    else{
+        const aPosition = inputText.search("@");
+        const afterA = inputText.slice(aPosition);
+        if(afterA == "")
+            displayMessage(`please enter a part following '@', "${input.value}" is empty`,"danger");
+        else{
+            displayMessage(`email added`,"success");
+        }
+    }
+
+})
+
+allLinks.forEach(function(item){
+    item.addEventListener("click",function(e){
+        e.preventDefault();
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        if (element === null)
+            return 0 
+        const navHeight = navigation.getBoundingClientRect().height;
+        const linkContainerHeight = links.getBoundingClientRect().height;
+        const fixedNav = navigation.classList.contains("fixed-nav");
+        let position= element.offsetTop-navHeight;
+        
+        if(!fixedNav)
+        {
+            console.log("if statement entered");
+            position = position - navHeight;  
+        }
+        if(links.getBoundingClientRect().height>80)
+        {
+            console.log("else if loop entered");
+            position = position + linkContainerHeight;
+        }
+        
+        window.scrollTo({
+            left:0,
+            top:position
+        })
+        links.style.height = `0px`;
+        containerHeight = 0;
+        isFirstClick = false;
+        
+    })
+})
+
+allLinks.forEach((item)=>{
+    item.addEventListener("click",()=>{
+      if(links.classList.contains("show-links")||links.style.height !== "0px")
+        {
+            links.style.height = `0px`;
+            containerHeight = 0;
+            links.classList.remove("show-links");
+    
+        }  
+    })
+})
+
+function displayMessage(message, type) {
+        alertMessage.style.display="inline";
+        alertMessage.classList.add(`alert-${type}`)
+        alertMessage.textContent = message;
+        setTimeout(function () {
+            alertMessage.textContent = "";
+            alertMessage.classList.remove(`alert-${type}`);
+            alertMessage.style.display="none";
+        }, 1000);
+    }
+
